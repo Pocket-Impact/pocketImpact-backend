17acde926b000288f86355eab5fa197e43d15cbe
wenger-dev <mugishawenger2@gmail.com>
2025-09-11 14:26:01 +0200

    removed unused

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { analyticsData, getDailyCategoriesData, getDashboardData, getOrganisationData } from '../controllers/dashboardController.js';
+import { getDailyCategoriesData, getDashboardData, getOrganisationData } from '../controllers/dashboardController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -11,7 +11,6 @@ router.get('/',protect, getDashboardData);
 router.get('/organisation', protect, getOrganisationData);
 
 // GET /api/dashboard/analytics - Get analytics data
-router.get('/analytics', protect, analyticsData);
 
 // GET /api/dashboard/daily-categories - Get daily categories data
 router.get('/daily-categories', protect, getDailyCategoriesData);
